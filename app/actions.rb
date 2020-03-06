03d8f7c70f18260e32085c6485dac02ce9570a16
Daniel Corner <danielcorner7@gmail.com>
2020-03-06 14:34:32 +0000

    set up fizzbizz

---
+++ app/actions.rb
@@ -0,0 +1,81 @@
+def get_humanized_time_ago(time_ago_in_minutes)
+  if time_ago_in_minutes >= 60
+    return "#{time_ago_in_minutes / 60} hours ago"
+  else
+    return "#{time_ago_in_minutes} minutes ago"
+  end
+end
+
+# get "/" do
+#     File.read('app/views/index.html')
+# end
+
+get '/' do
+
+  finstagram_post_shark = {
+    username: "sharky_j",
+    avatar_url: "http://naserca.com/images/sharky_j.jpg",
+    photo_url: "http://naserca.com/images/shark.jpg",
+    humanized_time_ago: get_humanized_time_ago(15),
+    like_count: 0,
+    comment_count: 1,
+    comments: [{
+      username: "sharky_j",
+      text: "Out for the long weekend... too embarrassed to show y'all the beach bod!"
+    }]
+  }
+
+  finstagram_post_whale = {
+    username: "kirk_whalum",
+    avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
+    photo_url: "http://naserca.com/images/whale.jpg",
+    humanized_time_ago: get_humanized_time_ago(65),
+    like_count: 0,
+    comment_count: 1,
+    comments: [{
+      username: "kirk_whalum",
+      text: "#weekendvibes"
+    }]
+  }
+
+  finstagram_post_marlin = {
+    username: "marlin_peppa",
+    avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
+    photo_url: "http://naserca.com/images/marlin.jpg",
+    humanized_time_ago: get_humanized_time_ago(190),
+    like_count: 0,
+    comment_count: 1,
+    comments: [{
+      username: "marlin_peppa",
+      text: "lunchtime! ;)"
+    }]
+  }
+
+   return [finstagram_post_shark, finstagram_post_whale, finstagram_post_marlin].to_s
+
+
+end
+
+get "/fizzbuzz" do 
+    my_string = ""
+    
+    for number in 1..100 do
+        is_divisible_by_3 = number % 3 == 0
+        is_divisible_by_5 = number % 5 == 0
+
+        if is_divisible_by_3 && is_divisible_by_5
+            my_string = my_string + "FizzBuzz <br/>"
+
+        elsif is_divisible_by_5
+            my_string = my_string + "Buzz <br/>"
+
+        elsif is_divisible_by_3 do
+            my_string = my_string + "Fizz <br/>"
+
+        else
+            my_string = my_string + "#{number} <br/>"
+        end
+    end
+    
+    return my_string
+end
