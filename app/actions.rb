9c2aecf0368b0a3194a045215e4b5e3fb595a56f
Daniel Corner <danielcorner7@gmail.com>
2020-03-11 19:10:54 +0000

    prep for W3D2

--- app/actions.rb
+++ app/actions.rb
@@ -12,7 +12,7 @@ end
 
 get '/' do
 
-  finstagram_post_shark = {
+  @finstagram_post_shark = {
     username: "sharky_j",
     avatar_url: "http://naserca.com/images/sharky_j.jpg",
     photo_url: "http://naserca.com/images/shark.jpg",
@@ -25,7 +25,7 @@ get '/' do
     }]
   }
 
-  finstagram_post_whale = {
+  @finstagram_post_whale = {
     username: "kirk_whalum",
     avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
     photo_url: "http://naserca.com/images/whale.jpg",
@@ -38,7 +38,7 @@ get '/' do
     }]
   }
 
-  finstagram_post_marlin = {
+  @finstagram_post_marlin = {
     username: "marlin_peppa",
     avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
     photo_url: "http://naserca.com/images/marlin.jpg",
@@ -51,15 +51,27 @@ get '/' do
     }]
   }
 
-   return [finstagram_post_shark, finstagram_post_whale, finstagram_post_marlin].to_s
+  #  my_variable is a normal variable, only accessible inside this code block
+  # @my_variable is an INSTANCE variable, accessible inside any .erb file we render
+  # -> "@" is like "pipe into .erb file"
+  @finstagram_posts = [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
 
+#   render the .erb file in /views named "index.erb"
+    erb(:index)
+
+    # final step: DRY the code https://web-pt.compass.lighthouselabs.ca/days/w3d1/activities/85
 
 end
+
+# next code group here https://web-pt.compass.lighthouselabs.ca/activities/35
 
 get "/fizzbuzz" do 
     my_string = ""
     
     for number in 1..100 do
+        # creating named booleans like these
+        # helps to verify that they're working
+        # (you can log the output to check)
         is_divisible_by_3 = number % 3 == 0
         is_divisible_by_5 = number % 5 == 0
 
@@ -69,11 +81,12 @@ get "/fizzbuzz" do
         elsif is_divisible_by_5
             my_string = my_string + "Buzz <br/>"
 
-        elsif is_divisible_by_3 do
+        elsif is_divisible_by_3
             my_string = my_string + "Fizz <br/>"
-
+            
         else
             my_string = my_string + "#{number} <br/>"
+            
         end
     end
     
